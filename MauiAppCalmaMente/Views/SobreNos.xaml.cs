b66ff5a07d2f4a7487840414ca16cd0d2b437718
Felipe Oliveira da Silva <123776069+fesilvags@users.noreply.github.com>
2026-04-30 20:37:25 -0300

    Sobre Nós

---
+++ MauiAppCalmaMente/Views/SobreNos.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppCalmaMente.Views;
+public partial class SobreNosPage : ContentPage
+{
+    public SobreNosPage()
+    {
+        InitializeComponent();
+        NavigationPage.SetHasNavigationBar(this, false);
+    }
+}
