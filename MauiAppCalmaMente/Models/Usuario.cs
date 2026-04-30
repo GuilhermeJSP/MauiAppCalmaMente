dc73236d3d89a2e67248fe2830dfcc977cdc5128
Felipe Oliveira da Silva <123776069+fesilvags@users.noreply.github.com>
2026-04-30 20:36:44 -0300

    Inclusão da models de Usuarios

---
+++ MauiAppCalmaMente/Models/Usuario.cs
@@ -0,0 +1,9 @@
+﻿using SQLite;
+namespace MauiAppCalmaMente.Models;
+public class Usuario
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public string Nome { get; set; } = "";
+    public string Senha { get; set; } = "";
+}
