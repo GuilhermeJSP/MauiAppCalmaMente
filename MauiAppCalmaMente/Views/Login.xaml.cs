8cc9220eb73870f0618746caaf2550070c24ae4f
Felipe Oliveira da Silva <123776069+fesilvags@users.noreply.github.com>
2026-05-01 18:13:24 -0300

    Update Login.xaml.cs
    Login funcional adicionado e código comentado

--- MauiAppCalmaMente/Views/Login.xaml.cs
+++ MauiAppCalmaMente/Views/Login.xaml.cs
@@ -1,35 +1,48 @@
 namespace MauiAppCalmaMente.Views;
 public partial class Login : ContentPage
 {
-    public Login(){
-        InitializeComponent();
-        NavigationPage.SetHasNavigationBar(this, false);
-            }
-
-private async void OnEntrar(object sender, EventArgs e)
+    public Login()
     {
+        InitializeComponent();
+
+        //Remove a barra mais escura presente na parte superior do app
+        NavigationPage.SetHasNavigationBar(this, false);
+
+    }
+
+    private async void OnEntrar(object sender, EventArgs e)
+    {
+        //Coleta os dados inseridos nas caixas de senha e usuario
         string usuario = entryUsuario.Text;
         string senha = entrySenha.Text;
 
+
+        //Verifica se os campos foram preenchidos 
         if (string.IsNullOrWhiteSpace(usuario) || string.IsNullOrWhiteSpace(senha))
         {
-            await DisplayAlert("AtenÁ„o", "Preencha todos os campos!", "OK");
+            await DisplayAlert("Aten√ß√£o", "Preencha todos os campos!", "OK");
             return;
         }
 
-        // Por enquanto valida usu·rio fixo ó depois pode conectar ao banco!
-        if (usuario == "admin" && senha == "1234")
+        //Consulta o banco de dados para ver se o usuario e a senha existem.
+        var usuarioEncontrado = await App.Database.BuscarUsuario(usuario, senha);
+        if (usuarioEncontrado != null)
         {
-            await Navigation.PushAsync(new InicioPage());
+
+            //se encontrar o usuario ele redireciona para a tela de inicio.
+            await Navigation.PushAsync(new InicioPage(usuarioEncontrado.Nome));
         }
         else
         {
-            await DisplayAlert("Erro", "Usu·rio ou senha incorretos!", "OK");
+            await DisplayAlert("Erro", "Usu√°rio ou senha incorretos!", "OK");
         }
     }
+
+    //Redireciona para a tela de cria√ß√£o de um novo usuario.
     private async void OnCadastrar(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new CadastroPage());
     }
+
 
 }
